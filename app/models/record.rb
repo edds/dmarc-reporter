e895db595f2aee9e19d1fa9e9c3441fc1acf094c
Edd Sowden <e@e26.co.uk>
2012-10-06 15:31:03 +0000

    Update cache counts on when saving records

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,10 @@ class Record < ActiveRecord::Base
   attr_accessible :comment, :count, :disposition, :dkim, :dkim, :dkim_domain, :dkim_hresult, :dkim_result, :header_from, :report_id, :source_ip, :spf, :spf_domain, :spf_result, :reson_type
 
   belongs_to :report
+
+  after_save :update_report_counts
+
+  def update_report_counts
+    self.report.update_counts
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,13 @@ class Report < ActiveRecord::Base
 
   has_many :records
   has_one :policy_published
+
+  def update_counts
+    counts = self.records.inject({failed: 0, passed: 0}) do |sum, record|
+      key = (record.disposition == 'reject') ? :failed : :passed
+      sum[key] = sum[key] + record.count
+      sum
+    end
+    self.update_attributes!({failed_count: counts[:failed], passed_count: counts[:passed]})
+  end
 end
