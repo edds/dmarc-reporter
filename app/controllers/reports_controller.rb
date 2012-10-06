03d51da26bfcbff4ff322c0febbabf1fb59f80b6
Edd Sowden <e@e26.co.uk>
2012-10-06 13:22:24 +0000

    Sort for newest at the top

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,13 @@
 class ReportsController < ApplicationController
 
   def index
-    @reports = Report.order(:created_at).page(params[:page])
+    @reports = Report.order("created_at desc").page(params[:page])
   end
 
   def show
     @report = Report.find(params[:id])
     @policy_published = @report.policy_published
-    @records = @report.records.order(:count).page(params[:page])
+    @records = @report.records.order("count desc").page(params[:page])
   end
 
 end
