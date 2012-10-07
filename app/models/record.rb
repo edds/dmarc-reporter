ef88547d4d2731ddc599c4ab4be461a951389e2a
Edd Sowden <e@e26.co.uk>
2012-10-07 10:08:57 +0000

    Only update counts if report exists

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,11 +1,11 @@
 class Record < ActiveRecord::Base
-  attr_accessible :comment, :count, :disposition, :dkim, :dkim, :dkim_domain, :dkim_hresult, :dkim_result, :header_from, :report_id, :source_ip, :spf, :spf_domain, :spf_result, :reson_type
+  attr_accessible :comment, :count, :disposition, :dkim, :dkim, :dkim_domain, :dkim_hresult, :dkim_result, :header_from, :report_id, :source_ip, :spf, :spf_domain, :spf_result, :reason_type
 
   belongs_to :report
 
   after_save :update_report_counts
 
   def update_report_counts
-    self.report.update_counts
+    report.update_counts if report
   end
 end
