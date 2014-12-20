116bae4ebba40f5005ca9af1220ed0cfc62fb4e2
Jörg Thalheim <joerg@higgsboson.tk>
2014-12-20 22:29:36 +0100

    EmailController: move persistence methods to model

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -5,11 +5,7 @@ class EmailController < ApplicationController
     dmarc = Dmarc::Email.new(params[:message]).report
     return unless dmarc
 
-    report = Report.create(dmarc.metadata)
-    report.create_policy_published(dmarc.policy_published)
-    dmarc.records.each do |row|
-      report.records << Record.create(row)
-    end
+    Record.save_dmarc(dmarc)
 
     render text: 'success', status: 200
   end
--- app/models/record.rb
+++ app/models/record.rb
@@ -3,6 +3,14 @@ class Record < ActiveRecord::Base
 
   after_save :update_report_counts
 
+  def self.save_dmarc(dmarc)
+    report = Report.create(dmarc.metadata)
+    report.create_policy_published(dmarc.policy_published)
+    dmarc.records.each do |row|
+      report.records << Record.create(row)
+    end
+  end
+
   def update_report_counts
     report.update_counts if report
   end
